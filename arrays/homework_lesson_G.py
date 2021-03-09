6c9e73bc6389540d8982ad14361df2dea2aeff96
Ivanm <ivanm2114@mail.ru>
2021-03-09 13:25:22 +0300

    G homework added

--- arrays/homework_lesson_G.py
+++ arrays/homework_lesson_G.py
@@ -1,9 +1,19 @@
 n = int(input())
 
-arr = []
+arr = [int(input())]
 
-for i in range(n):
-    arr.append(int(input()))
+count = 1
+maximum = arr[0]
+for i in range(n - 1):
+    x = int(input())
+    arr.append(x)
+    if x > maximum:
+        maximum = x
+        count = 1
+    elif x == maximum:
+        count += 1
 
+
+print(count)
 maxNum = max(arr)
 print(arr.count(maxNum))

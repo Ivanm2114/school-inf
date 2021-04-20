202383fd75d8e2aebb80a88cff89d7c686887bc9
Ivanm <ivanm2114@mail.ru>
2021-04-20 13:43:41 +0300

    work on 20.04 added

--- work_with_files/1.py
+++ work_with_files/1.py
@@ -1,5 +1,12 @@
 fin = open('in1.txt')
 summ = 0
+count = 0
 for line in fin.readlines():
     summ += int(line)
+    count += 1
+fout = open('out1.txt', 'w')
+fout.write(str(summ) + '\n' + str(count) + '\n' + str(summ/count))
 print(summ)
+print(count)
+fin.close()
+fout.close()

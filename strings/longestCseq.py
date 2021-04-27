7172f12fc9ed22bf3f9d172aeb07e1482b564f3c
Ivanm <ivanm2114@mail.ru>
2021-04-27 12:54:23 +0300

    while changed to for

--- strings/longestCseq.py
+++ strings/longestCseq.py
@@ -2,12 +2,11 @@ s = input()
 i = 0
 maximum = 0
 count = 0
-while i < len(s) - 1:
+for i in range(0, len(s) - 1):
     if s[i] == s[i + 1] == 'C':
         count += 1
     else:
         maximum = max(maximum, count + 1)
         count = 0
-    i += 1
 maximum = max(maximum, count + 1)
 print(maximum)
